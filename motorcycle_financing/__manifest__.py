c739547c4ae25ec4b448f952ad764a777d1de9ac
Amir Ali <info@unifiedtechnology.ca>
2025-02-25 16:28:13 -0700

    update: Motor_Cycle_Module update: Views and Menu

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -1,12 +1,11 @@
 {
-    "name": "Motorcycle Financing",
-    "summary": "Streamlines the loan application process for dealershops",
-    "version": "18.0.0.0.1",
-    "license": "OPL-1",
-    "category": "Kawiil/Motorcycle",
-    "author": "Unified ERP Consultant",
-    "Application": "True",
-    "data": [
+    'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealershops',
+    'version': '18.0.0.0.1',
+    'license': 'OPL-1',
+    'category': 'Kawiil/Motorcycle',
+    'author': 'Unified ERP Consultant',
+    'data': [
         #security
         'security/res_groups.xml',
         'security/ir.model.access.csv',
@@ -15,7 +14,9 @@
         'views/motorcycle_financing_menu.xml',
         #Menu
     ],
-    "demo": [
-        "demo/demo.xml"
-    ]
+    'demo': [
+        'demo/demo.xml'
+    ],
+    'application': 'True',
+    'installable': 'True',
 }
